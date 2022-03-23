52f2c89d234293f42636afb894ddc2fb90cd11c6
Luc <ludan1214@gmail.com>
2022-03-22 21:26:56 -0500

    Update Role.java

--- src/main/java/com/koala/linkfilterapp/linkfilterapp/security/entity/Role.java
+++ src/main/java/com/koala/linkfilterapp/linkfilterapp/security/entity/Role.java
@@ -1,17 +1,19 @@
 package com.koala.linkfilterapp.linkfilterapp.security.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
-@Data
 public class Role implements Serializable {
-    private static final long serialVersionUID = 1L;
     public static final String USER = "USER";
     public static final String ROLE_USER = "ROLE_USER";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
@@ -30,5 +32,38 @@ public class Role implements Serializable {
 
     public Role(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Role role = (Role) obj;
+        if (!role.equals(role.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Role [name=").append(name).append("]").append("[id=").append(roleId).append("]");
+        return builder.toString();
     }
 }
