a3c46b69f637f26580f6d29b2fbe62f60d456533
Luc <ludan1214@gmail.com>
2022-04-18 23:16:47 -0500

    Update User.java

--- src/main/java/com/koala/linkfilterapp/linkfilterapp/security/entity/User.java
+++ src/main/java/com/koala/linkfilterapp/linkfilterapp/security/entity/User.java
@@ -43,7 +43,6 @@ public class User implements Serializable {
     private String provider;
 
     // bi-directional many-to-many association to Role
-//    @JsonIgnore
     @ManyToMany
     @JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "USER_ID") }, inverseJoinColumns = { @JoinColumn(name = "ROLE_ID") })
     private Set<Role> roles;
