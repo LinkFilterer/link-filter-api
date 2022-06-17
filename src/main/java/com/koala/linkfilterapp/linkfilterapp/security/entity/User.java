377cbc73e81ae73c92d7e87bb6c5ce748a9d89ca
Luc <ludan1214@gmail.com>
2022-06-16 20:29:19 -0500

    Update User.java

--- src/main/java/com/koala/linkfilterapp/linkfilterapp/security/entity/User.java
+++ src/main/java/com/koala/linkfilterapp/linkfilterapp/security/entity/User.java
@@ -12,37 +12,29 @@ import java.util.Set;
 @NoArgsConstructor
 @Entity
 public class User implements Serializable {
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date modifiedDate;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     Long id;
-
     @Column(name = "PROVIDER_USER_ID")
     String providerUserId;
-
     String email;
-
     String ipAddress;
-
     @Column(name = "enabled", columnDefinition = "BIT", length = 1)
     boolean enabled;
-
     @Column(name = "DISPLAY_NAME")
     String displayName;
-
-    @Column(name = "created_date", nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    protected Date modifiedDate;
-
     String password;
 
     String provider;
 
     // bi-directional many-to-many association to Role
     @ManyToMany(cascade = CascadeType.DETACH)
-    @JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "USER_ID") }, inverseJoinColumns = { @JoinColumn(name = "ROLE_ID") })
+    @JoinTable(name = "user_role", joinColumns = {@JoinColumn(name = "USER_ID")}, inverseJoinColumns = {@JoinColumn(name = "ROLE_ID")})
     Set<Role> roles;
 }
